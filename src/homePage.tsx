ebccf4d0eb74dd75edbd969a34f53466425b9fb3
David Nellis <dnellis74@gmail.com>
2024-06-24 16:21:56 -0600

    Got data grid working

--- src/homePage.tsx
+++ src/homePage.tsx
@@ -1,42 +1,105 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
 import { useNavigate } from 'react-router-dom';
+import { useEffect, useMemo, useState } from 'react';
+
+import { AgGridReact } from '@ag-grid-community/react'; // React Grid Logic
+import "@ag-grid-community/styles/ag-grid.css"; // Core CSS
+import "@ag-grid-community/styles/ag-theme-material.css"; // Theme
+
+import { ColDef, ModuleRegistry } from '@ag-grid-community/core';
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
+
+ModuleRegistry.registerModules([ClientSideRowModelModule]);
 
 /*eslint-disable*/
-function parseJwt (token) {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-    return JSON.parse(jsonPayload);
+function parseJwt(token) {
+  var base64Url = token.split('.')[1];
+  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+  return JSON.parse(jsonPayload);
+}
+
+// Row Data Interface
+interface IRow {
+  mission: string;
+  company: string;
+  location: string;
+  date: string;
+  time: string;
+  rocket: string;
+  price: number;
+  successful: boolean;
+}
+
+const GridExample = () => {
+  // Row Data: The data to be displayed.
+  const [rowData, setRowData] = useState<IRow[]>([]);
+
+  // Column Definitions: Defines & controls grid columns.
+  const [colDefs] = useState<ColDef[]>([
+    { field: "mission" },
+    { field: "company" },
+    { field: "location" },
+    { field: "date" },
+    { field: "price" },
+    { field: "successful" },
+    { field: "rocket" }
+  ]);
+
+  // Apply settings across all columns
+  const defaultColDef = useMemo<ColDef>(() => {
+    return {
+      filter: true
+    };
+  }, []);
+
+  // Fetch data & update rowData state
+  useEffect(() => {
+    fetch('https://www.ag-grid.com/example-assets/space-mission-data.json') // Fetch data from server
+      .then(result => result.json()) // Convert to JSON
+      .then(rowData => setRowData(rowData)) // Update state of `rowData`
+  }, [])
+
+  // Container: Defines the grid's theme & dimensions.
+  return (
+    <div className={"ag-theme-material"} style={{ width: 1048, height: 500 }}>
+      {/* The AG Grid component, with Row Data & Column Definition props */}
+      <AgGridReact
+        rowData={rowData}
+        columnDefs={colDefs}
+        defaultColDef={defaultColDef}
+        pagination={true}
+      />
+    </div>
+  );
 }
 
 const HomePage = () => {
   const navigate = useNavigate();
   var idToken = parseJwt(sessionStorage.idToken.toString());
   var accessToken = parseJwt(sessionStorage.accessToken.toString());
-  console.log ("Amazon Cognito ID token encoded: " + sessionStorage.idToken.toString());
-  console.log ("Amazon Cognito ID token decoded: ");
-  console.log ( idToken );
-  console.log ("Amazon Cognito access token encoded: " + sessionStorage.accessToken.toString());
-  console.log ("Amazon Cognito access token decoded: ");
-  console.log ( accessToken );
-  console.log ("Amazon Cognito refresh token: ");
-  console.log ( sessionStorage.refreshToken );
-  console.log ("Amazon Cognito example application. Not for use in production applications.");
+  console.log("Amazon Cognito ID token encoded: " + sessionStorage.idToken.toString());
+  console.log("Amazon Cognito ID token decoded: ");
+  console.log(idToken);
+  console.log("Amazon Cognito access token encoded: " + sessionStorage.accessToken.toString());
+  console.log("Amazon Cognito access token decoded: ");
+  console.log(accessToken);
+  console.log("Amazon Cognito refresh token: ");
+  console.log(sessionStorage.refreshToken);
+  console.log("Amazon Cognito example application. Not for use in production applications.");
   const handleLogout = () => {
     sessionStorage.clear();
     navigate('/login');
   };
-/*eslint-enable*/
 
   return (
     <div>
-      <h1>Hello World</h1>
-      <p>See console log for Amazon Cognito user tokens.</p>
+      <h1>Policy Intelligence</h1>
+      <GridExample />
+
       <button onClick={handleLogout}>Logout</button>
+      <p>See console log for Amazon Cognito user tokens.</p>
     </div>
   );
 };
