4d39a249ef002d35f68a780d3e94e30bc5d9556c
David Nellis <dnellis74@gmail.com>
2024-10-07 14:45:10 -0600

    moved things to make mono repo

--- utils/scrape/runner.py
+++
@@ -1,8 +0,0 @@
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-from scrape.spiders.apcd_onbase_spider import ApcdOnBaseSpider
-from scrape.spiders.dave_apcd_scrape import ApcdScrape  # Replace with your spider's import
-
-process = CrawlerProcess(get_project_settings())
-process.crawl(ApcdScrape)
-process.start()
